3afc2e370ab054c4461c9b86f22345b58970bb25
suhaibmtz <suhaibmtz68@gmail.com>
2024-12-20 15:43:21 +0200

    remaking mod basicly

--- cannon.lua
+++ cannon.lua
@@ -1,110 +1,88 @@
--- idea: there is a chance that it will explode in ur face
--- maybe: make an entity to make it look better just maybe
 
 sin = math.sin
 cos = math.cos
 rad = math.rad
 
+local mortar = {
+    initial_properties = {
+        visual = "mesh",
+        mesh = "mortar_cannon.obj",
+        textures = {"default_steel_block.png"},
+        collisionbox = {-0.5, 0, -0.5, 0.5, 1.5, 0.5},
+		physical = true,
+        visual_size = {x = 1, y = 1, z = 1},
+    },
 
-extra = ""
-top = "default_steel_block.png^default_coal_lump.png"
-top_onlit = "default_steel_block.png^default_flint.png"
-if conduct then
-	extra = "smart"
-	top = "default_steel_block.png^default_mese_crystal.png"
-	top_onlit = "default_steel_block.png^default_mese_crystal.png"
+	power = 20,
+	angle = 70,
+}
+
+function mortar:on_punch(puncher, time_from_last_punch, tool_capabilities, direction,
+	damage)
+	cont = puncher:get_player_control()
+	if cont.sneak then
+		local yaw = puncher:get_look_horizontal()
+
+		local rotation = self.object:get_rotation()
+        if rotation then
+            rotation.y = yaw
+            self.object:set_rotation(rotation)
+        end
+	elseif cont.aux1 then
+		pos = self.object:get_pos()
+		self.object:remove()
+		inv = puncher:get_inventory()
+		item = ItemStack("mortar:cannon")
+		if inv:room_for_item("main",item) then
+			inv:add_item("main",item)
+		else
+			minetest.item_drop(item, puncher, pos)
+		end
+	else
+		item = puncher:get_wielded_item()
+		if item:get_name() == "mortar:bomb" then
+			inv = puncher:get_inventory()
+			inv:remove_item("main", ItemStack("mortar:bomb"))
+			rot = math.deg(self.object:get_rotation().y)
+			rot = math.fmod(rot+90,360)
+			shoot(self.object:get_pos(),rad(rot),self.angle,self.power)
+		end
+	end
+
+	return true
 end
-minetest.register_node("mortar:cannon"..extra,{
-	description = "cannon "..extra,
-	tiles = {
-		top,
-		"default_steel_block.png",
-		"default_steel_block.png",
-		"default_steel_block.png",
-		"default_steel_block.png^default_flint.png",
-		"default_steel_block.png",
-	},
-	stack_max = 1,
-	is_ground_content = false,
-	paramtype2 = "facedir",
-	groups = {
-		cracky = 3,
-		oddly_breakable_by_hand = 1,
-	},
-        after_place_node = function(pos, placer, itemstack, pointed_thing)
-        	local meta = minetest.get_meta(pos)
-        	local inv = meta:get_inventory()
-    		inv:set_size("main", 32)
-    		meta:set_int("power",40)
-    		meta:set_int("angle",70)
-        end,
-	on_punch = function(pos, node, player, pointed_thing)
-            item = player:get_wielded_item()
-            if item:get_name() == "mortar:bomb" then
-            	meta = minetest.get_meta(pos)
-            	angle = meta:get_int("angle")
-        	power = meta:get_int("power")
-            	inv = player:get_inventory()
-            	inv:remove_item("main",ItemStack("mortar:bomb"))
-            	minetest.set_node(pos,{name="mortar:cannon_loaded",param2=node.param2})
-            	meta = minetest.get_meta(pos)
-            	meta:set_int("angle",angle)
-        	meta:set_int("power",power)
-            	minetest.get_node_timer(pos):start(0.5)
-            end
-        end,
-        on_rightclick = function(pos, node, clicker, itemstack, pointed_thing)
-        	meta = minetest.get_meta(pos)
-        	angle = meta:get_int("angle")
-        	power = meta:get_int("power")
-        	formname = minetest.serialize({name="cannon",pos=pos})
-        	minetest.show_formspec(clicker:get_player_name(), formname, "size[3,2.5]"..
-			"field[0.6,0.5;2.5,0.6;angle;Angle;"..angle.."]"..
-			"field[0.6,1.8;2.5,0;power;Power;"..power.."]"..
-			"button[0.8,1.7;1.5,1.6;save;Save]")
-        end,
-})
 
-minetest.register_node("mortar:cannon"..extra.."_loaded",{
-	description = "cannon "..extra,
-	tiles = {
-		"default_steel_block.png^default_flint.png",
-		"default_steel_block.png",
-		"default_steel_block.png",
-		"default_steel_block.png",
-		top_onlit,
-		"default_steel_block.png",
-	},
-	stack_max = 1,
-	is_ground_content = false,
-	paramtype2 = "facedir",
-	groups = {
-		cracky = 3,
-		oddly_breakable_by_hand = 1,
-             	not_in_creative_inventory = 1,
-	},
-	can_dig = function(pos, player)
-            return false
-        end,
-        on_timer = function(pos, elapsed)
-    		meta = minetest.get_meta(pos)
-        	rot = meta:get_int("angle")
-        	r = rad(rot)
-        	p = meta:get_int("power")
-        
-        	node = minetest.get_node(pos)
-        	dir = minetest.facedir_to_dir(node.param2)
-        	entity = minetest.add_entity({x=pos.x+dir.x,y=pos.y+1,z=pos.z+dir.z},"ww1_planes_lib:bomb1")
-        	entity:set_velocity({x=cos(r)*dir.x*p,y=sin(r)*p,z=cos(r)*dir.z*p})
-        	minetest.set_node(pos,{name="mortar:cannon",param2=node.param2})
-        	--minetest.get_node_timer(pos):start(0.2)
-		minetest.sound_play("tnt_explode", {pos = pos, gain = 2.5,
-			max_hear_distance = math.min(25 * 20, 128)}, true)
-            	meta = minetest.get_meta(pos)
-            	meta:set_int("angle",rot)
-        	meta:set_int("power",p)
-        end,
-})
+function mortar:on_rightclick(clicker)
+	clicker:set_attach(self.object)
+	angle = self.angle
+	power = self.power
+
+	minetest.show_formspec(clicker:get_player_name(), "mortar", "size[2,2.6]"..
+		"field[0.55,0.4;1.5,1;power;Power;"..power.."]"..
+		"field[0.55,1.5;1.5,1;angle;Angle;"..angle.."]"..
+		"button[0.25,2.15;1.54,1;save;Save]")
+end
+
+minetest.register_entity("mortar:cannon",mortar)
+
+function shoot(pos,r,a,p)
+	a = rad(a)
+    node = minetest.get_node(pos)
+    
+	dir = {x=cos(r),y=1,z=sin(r)}
+	local magnitude = math.sqrt(dir.x^2 + dir.y^2 + dir.z^2)
+	dir.x = dir.x / magnitude
+	dir.y = dir.y / magnitude
+	dir.z = dir.z / magnitude
+    
+	velocity = vector.multiply({x=cos(a),y=sin(a),z=cos(a)},dir)
+	velocity = vector.multiply(velocity,p)
+
+    entity = minetest.add_entity({x=pos.x,y=pos.y+1,z=pos.z},"ww1_planes_lib:bomb1")
+    entity:set_velocity(velocity)
+	minetest.sound_play("tnt_explode", {pos = pos, gain = 2.5,
+	x_hear_distance = math.min(25 * 20, 128)}, true)
+end
 
 minetest.register_craftitem("mortar:bomb",{
 	description = "cannon bomb",
@@ -112,18 +90,34 @@ minetest.register_craftitem("mortar:bomb",{
 	inventory_image = "default_flint.png^default_coal_lump.png",
 })
 
+minetest.register_craftitem("mortar:cannon",{
+	description = "mortar cannon",
+	stack_max = 1,
+	inventory_image = "mortar_cannon.png",
+	on_place = function (itemstack, user, pointed_thing)
+		if pointed_thing.type == "node" then
+			pos = pointed_thing.above
+			obj = minetest.add_entity(pos,"mortar:cannon")
+			if obj then
+				local yaw = user:get_look_horizontal()
+				obj:set_rotation({x=0,y=yaw,z=0})
+				itemstack:take_item()
+				return itemstack
+			end
+		end
+	end
+})
+
+
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-    if string.sub(formname,1,6) ~= "return" then
+    if formname ~= "mortar" then
         return
     end
 
     if fields.save then
-    	info = minetest.deserialize(formname)
-    	if info.name ~= "cannon" then
-    		return
-    	end
+		obj = player:get_attach()
+		ent = obj:get_luaentity()
     	
-    	meta = minetest.get_meta(info.pos)
     	angle = tonumber(fields.angle)
     	if angle == nil then
     		angle = 65
@@ -136,7 +130,9 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
     	if power == nil then
     		power = 20
     	end
-    	meta:set_int("angle",angle)
-    	meta:set_int("power",power)
+    	ent.angle = angle
+    	ent.power = power
     end
+	player:set_detach()
+	minetest.close_formspec(player:get_player_name(), formname)
 end)
